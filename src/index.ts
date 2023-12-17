f043deee5a6d8e5916b68788db817ffab4a9758e
viniciszs <114860862+viniciszs@users.noreply.github.com>
2023-12-17 18:08:30 -0300

    delete vercel

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import express from 'express'
 import cors from 'cors'
 
-import prisma from './external/db'
-
 import { routerDiaT } from './routers/DiaTrabalhado'
 
 const port = process.env.PORT || 8182
@@ -13,9 +11,6 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/diaTrabalhado', routerDiaT)
-app.get('/', async (req, res) => {
-  res.send(await prisma.diaTrabalhado.findMany())
-})
 
 
 app.listen(port, () => {
